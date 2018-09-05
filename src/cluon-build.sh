8801d604d367ac8398fe709075b222a5a4855ca2
Adam2092 <adam.2092@gmail.com>
2018-09-05 22:33:00 +0200

    No longer need .cpp file for messages after libcluon v0.0.104

--- src/cluon-build.sh
+++ src/cluon-build.sh
@@ -21,10 +21,7 @@ echo "Now compiling cluon-msc..."
 g++ -DHAVE_CLUON_MSC -std=c++14 -pthread -o cluon-msc cluon-complete.cpp
 
 echo "Now generating $1.hpp..."
-./cluon-msc --cpp-headers --out=$1.hpp $1.odvd
-
-echo "Now generating $1.cpp..."
-./cluon-msc --cpp-sources --out=$1.cpp --cpp-add-include-file=$1.hpp $1.odvd
+./cluon-msc --cpp --out=$1.hpp $1.odvd
 
 echo "Now cleaning up..."
 rm -f cluon-msc cluon-complete.cpp
